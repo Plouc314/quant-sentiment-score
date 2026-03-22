22db66561bfdef280eedf2bffb6b6e4e6f478156
Alexandre Goumaz <51334065+Plouc314@users.noreply.github.com>
2026-03-22 15:59:26 +0100

    use kaggle dataset (#6)

--- sentiment/sources/news/__init__.py
+++ sentiment/sources/news/__init__.py
@@ -1,6 +1,7 @@
 from .extractor import ArticleExtractor
+from .kaggle import KaggleImporter
 from .models import Article, Story
 from .repository import ArticleRepository
 from .search import NewsSearch
 
-__all__ = ["Article", "ArticleExtractor", "ArticleRepository", "NewsSearch", "Story"]
+__all__ = ["Article", "ArticleExtractor", "ArticleRepository", "KaggleImporter", "NewsSearch", "Story"]
